0a3c5e1ae7ae0ed55397303217de54253bc69123
Lincest <474617032@qq.com>
2019-11-03 19:50:37 +0800

    HFM_TREE

--- HFM_Tree/HFM_Tree.cpp
+++ HFM_Tree/HFM_Tree.cpp
@@ -1,5 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
+const int N=500;
+map<char, string>M;//利用map存储字符和哈夫曼编码之间的关系 
 
 typedef struct //hfm树的节点 
 {
@@ -36,38 +38,35 @@ void CreateTree(node *Htree, int n)	//创建哈夫曼树,n表示结点个数
         //printf("第%d次找到了now ： %d \n",i,Htree[min1].weight + Htree[min2].weight);
         Htree[now].weight = Htree[min1].weight + Htree[min2].weight;
         Htree[now].l = min1, Htree[now].r = min2;
+        Htree[now].c = '#';
         Htree[min1].par = now, Htree[min2].par = now;
         now++;
         m++;
     }
 }
 
-char* ReadFile(char *s)	//文件的读取 
-{
-	FILE* FileInput = fopen(s,"r");
-	char ReadBuffer[1024] = {0};
-	if (FileInput)
-	{
-	    fread(ReadBuffer,1,1024,FileInput);
-	    return ReadBuffer;
-	    fclose(FileInput);
-	}
-	return "file-read error";
-}
-
-
-  //打印树
+//打印树
 void print(node *t,int next)
 {
-    printf("%d\n",t[next].weight);
-    if(t[next].l)
-        print(t, t[next].l);
-    if(t[next].r)
-        print(t, t[next].r);
+	queue<node> q;
+	q.push(t[next]);
+	node out;
+	FILE *fp;
+	fp = fopen("hfmTree.txt", "w");
+	while(!q.empty()){
+		out=q.front();
+		q.pop();
+	    fprintf(fp,"%c %d	",out.c,out.weight);
+	    if(out.l&&out.r){
+	        q.push(t[out.l]);
+	        q.push(t[out.r]);
+	    }
+	}
+	cout<<"\n已将建立的哈夫曼树写入hfmTree.txt文件中\n"<<endl;
+	fclose(fp);
 }
 
-
-void Initialization(char* input_c,int *input_w,int len)	//len表示字符的个数 
+node* Initialization(char* input_c,int *input_w,int len)	//len表示字符的个数 
 {
 	node* Htree;
 	Htree = (node*)malloc((2*len)*sizeof(node));
@@ -79,14 +78,120 @@ void Initialization(char* input_c,int *input_w,int len)	//len表示字符的个数
 	} 
 	CreateTree(Htree,len);
 	print(Htree,2*len-1);
+	return Htree;
 }
 
+string ReadFile()	//读取CodeFile文件 
+{
+	ifstream in("CodeFile.txt");
+	string out;
+	in>>out;
+	return out;
+	in.close();
+}
+
+void Encode(node *Htree, int n)	//len=2n-1 
+{
+    char temp[N];//临时存放某个叶子节点的哈夫曼编码
+    int now = N-1;
+    //逆序推出哈夫曼编码，左边为0，右边为1
+    puts("编码规则为：");
+    for(int i = 1; i <= n; i++)	//从1号到n号依次推出哈夫曼编码 
+    {
+        now = N-1;
+        for(int j = i; Htree[j].par != 0; j = Htree[j].par)
+        {
+            int p = Htree[j].par;
+            if(Htree[p].l == j) temp[--now] = '0';
+            else temp[--now] = '1';
+        }
+        printf("%c : %s\n", Htree[i].c, temp+now);
+        string temp1(temp+now);
+        M[Htree[i].c] = temp1;
+    } 
+}
+
+//进行哈夫曼解码
+void Decode()
+{
+	string s = ReadFile();
+	FILE *fp = fopen("CodePrin.txt","w");
+    int len = s.size(), c = 0;
+    char str[N];
+    cout<<"对CodeFile的译码结果为："<<endl;
+    for(int i = 0; i < len; i++)
+    {
+        str[c++] = s[i];
+        str[c] = '\0';
+        for(map<char, string>::iterator it = M.begin(); it != M.end(); it++)
+        {
+            if(it->second == str)
+            {
+                printf("%c", it->first);
+                fprintf(fp,"%c", it->first);
+                c = 0;
+            }
+        }
+    }
+    cout<<"\n已将结果存入CodePrin.txt中"<<endl;
+    fclose(fp);
+}
+
+
+string Save_encodeFile(string str){	//输入内容为要加密的string 
+	string str_encode="";
+	for(int i=0;i<str.size();++i)
+	{
+		str_encode+=M[str[i]];
+	}
+	ofstream savefile("CodeFile.txt");
+    savefile<<str_encode;
+    savefile.close();
+    cout<<"str加密后为: "<<str_encode<<endl;
+    cout<<"已将加密后的内容写入文件CodeFile.txt中\n";
+    return str_encode;
+}
+
+
+
 int main(){
-	//char file[1024]="hfmTree.txt";
 	char input_char[100]=" ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int input_weight[100]={186,64,13,22,32,103,21,15,47,57,1,5,32,20,57,63,15,1,48,51,80,23,8,18,1,16,1};
-//	char input_char[100]="ABCDEFGH";
-//	int input_weight[100]={5,29,7,8,14,23,3,11};
-	Initialization(input_char,input_weight,27);
+	int n=27;	//n表示字符个数 
+	//char input_char[100]="ABCDEFGH";
+	//int input_weight[100]={5,29,7,8,14,23,3,11};
+	cout<<"字符频度为: \n";
+	for(int i=0;i<n;++i){
+		printf("%c : %d\n",input_char[i],input_weight[i]);
+	}
+	node* Htree = Initialization(input_char,input_weight,n);
+	Encode(Htree,n); //对哈夫曼树编码 
+	
+	
+	//以上为哈夫曼树的初始化 
+	
+	char x;
+	cout<<"\n进入哈夫曼树编码系统，按:\nQ 退出\nD 对CodeFile中文件译码并将内容存入CodePrin\nP 打印CodeFile中源码\nE 输入并编码内容至CodeFile文件\n\n\n" ;
+	while(scanf("%c",&x)&&x!='Q')
+	{
+			if(x=='D'){
+				Decode();
+				cout<<"\n\n";
+			}
+			else if(x=='P'){
+				string s = ReadFile();
+				cout<<s<<endl;
+				cout<<"\n\n";
+			}
+			else if(x=='E'){
+				cout<<"请输入需要加密的字符串: ";
+				string str;
+				getchar();
+				getline(cin,str);
+				string str_encode = Save_encodeFile(str);
+				cout<<"\n\n";
+			}
+	}
+
 	return 0;
 }
